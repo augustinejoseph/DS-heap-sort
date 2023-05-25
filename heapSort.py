eb330b7de7f7d7936f7adad187ee2a2b51d74d7f
augustinejoseph <augustine.augustine4@gmail.com>
2023-05-25 14:47:34 +0530

    added heapsort

--- heapSort.py
+++ heapSort.py
@@ -1,24 +1,36 @@
+# Heapify function
 def heapify(Arr, n, i):
-    left = 2*i +1
-    right = 2*i +2
     largest = i
-    if n > left and Arr[left] < Arr[largest]:
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and Arr[left] > Arr[largest]:
         largest = left
-    if n > right and Arr[right ] < Arr[largest]:
+
+    if right < n and Arr[right] > Arr[largest]:
         largest = right
-    if i != largest:
+
+    if largest != i:
         Arr[i], Arr[largest] = Arr[largest], Arr[i]
+        heapify(Arr, n, largest)
+
+# Heap sort
+def heapSort(Arr):
+    n = len(Arr)
+
+    for i in range(n // 2 - 1, -1, -1):
         heapify(Arr, n, i)
 
+    for i in range(n - 1, 0, -1):
+        Arr[0], Arr[i] = Arr[i], Arr[0]
+        heapify(Arr, i, 0)
 
-Arr = [2,66,30,5,9,10]
+Arr = [2, 66, 30, 5, 9, 10]
 n = len(Arr)
 
-# Heapify
-for i in range(n, -1,-1):
-    heapify(Arr, n, i)
+heapSort(Arr)
 
 # Printing the result
-print("Heaped array is:")
+print("Sorted array:")
 for i in range(n):
     print(Arr[i])
